cbd531ed6b65c7109b57d8da2eb52a3ff3acedde
Virendra Kumar Dwivedi <dwivedivirendrakumar23@gmail.com>
2025-10-06 22:33:31 +0530

    class2_ project2

---
+++ project2/src/App.js
@@ -0,0 +1,12 @@
+import Data from "/components/Data.js";
+import User from "./components/User.jsx";
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ project2/src/components/Data.js
@@ -0,0 +1,11 @@
+function Data(){
+    return 
+       (
+        <div>
+          this is a veer
+        </div>
+
+       )
+    
+}
+export default Data;
